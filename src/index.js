3ca50f9e6967733bf788e4782d5dbb983577daa2
昇爷 <zhs0518@outlook.com>
2015-04-24 16:02:14 +0800

    init
    初始化项目

---
+++ src/index.js
@@ -0,0 +1,54 @@
+var MyDate = function (date) {
+	// chagne the num by current date
+	function cacularDate(num) {
+		if (!num) {
+			return new Date();
+		}
+
+		if (typeof num !== 'number') {
+			if (isNaN(num)) {
+				return new Date();
+			}else {
+				num = parseInt(num);
+			}
+		}
+
+		var date = new Date();
+		date.setTime((new Date()).getTime() - num * 24000 * 3600);
+		return new Date(date.toString());
+	}
+
+	switch (date.replace(/\d/, '').replace(/\s/g, '')) {
+		case 'hoursago':
+		case 'hourago':
+			this.date = cacularDate(parseInt(date.match(/\d+/)[0]) / 24);
+			break;
+		case 'day': 
+		case 'yesterday': 
+			this.date = cacularDate(1);
+			break;
+		case 'daysago':
+			this.date = cacularDate(date.match(/\d+/)[0]);
+			break;
+		case 'weeksago':
+			this.date = cacularDate(parseInt(date.match(/\d+/)[0]) * 7);
+			break;
+		case 'monthsago':
+			this.date = cacularDate(parseInt(date.match(/\d+/)[0]) * 30);
+			break;
+		case 'yearago':
+		case 'yearsago':
+			this.date = cacularDate(parseInt(date.match(/\d+/)[0]) * 365);
+			break;
+		default:
+			this.date = new Date(date);
+			break;
+	}
+}
+
+MyDate.prototype.toString = function () {
+	var date = this.date;
+	return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+}
+
+exports.MyDate = MyDate
---
+++ test/test.js
@@ -0,0 +1,5 @@
+var MyDate = require('./../src/index').MyDate;
+
+var myDate = new MyDate('2015-4-10 15:56:41');
+
+console.log(myDate.toString());
